cfcabd56cbc9fec869850bf45f851d3896a4852c
hildegunn76 <77569969+hildegunn76@users.noreply.github.com>
2021-01-25 23:04:24 +0100

    Update script.js
    Updated

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+function compute() {
+		var Principal = document.getElementById('principal').value;		 
+		var Rate = document.getElementById('rate').value;
+	    var Years = document.getElementById('years').value;		 
+        var Interest = Principal * Years * Rate/100;
+
+        document.getElementById('result').innerHTML = PayoutYear(); 
+
+        var Result = "<br />If you deposit <span>" + Principal + "</span>,<br />" 
+
+        Result = Result + " at an interest rate of " + Rate + "%. <br />" 
+        Result = Result + " You will receive an amount of " + Interest + ",<br />" 
+        Result = Result + "in the year " + PayoutYear(); 
+
         
+
+        document.getElementById('result').innerHTML = Result
+
+		}
+         
+function PayoutYear()         { 
+    var Years = 0;
+    Years = document.getElementById('years').value;
+    return 2021  + parseInt(Years);
+
+}
+
+function UpdateSlider(){
+    var Rate = document.getElementById('rate').value;
+
+    document.getElementById('rateAsText').innerHTML = Rate;
+
+}
+
+
+
+
