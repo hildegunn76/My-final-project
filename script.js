83ac3e220fce0a81cf27d9cb320ab10259ae535c
hildegunn76 <77569969+hildegunn76@users.noreply.github.com>
2021-01-27 23:19:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compute() {
 
         		var Result = "<br />If you deposit <span>" + Principal + "</span>,<br />" 
 
-			Result = Result + " at an interest rate of <strong>" + Rate + "% </strong>. <br />" 
+			Result = Result + " at an interest rate of <span>" + Rate + "% </span>. <br />" 
 			Result = Result + "You will receive an amount of <span>" + Interest + "</span>,<br />" 
 			Result = Result + "in the year <span>" + PayoutYear() + "</span>"; 
 
