41451697f21e454707d2c4b95e75b71e4b1ef21d
hildegunn76 <77569969+hildegunn76@users.noreply.github.com>
2021-01-27 23:11:56 +0100

    Update script.js
    s

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function compute() {
 		var Principal = document.getElementById('principal').value;		 
 		var Rate = document.getElementById('rate').value;
-	    var Years = document.getElementById('years').value;	
-	  	if (Principal <= 0) {
-    		alert "Enter a positive number";
-		document.getElementById("principal").focus();
-  		} else {
-        var Interest = (Principal * Years * Rate)/100;
+	    	var Years = document.getElementById('years').value;	
+	  	
+		if (Principal <= 0) {
+    			alert "Enter a positive number";
+			document.getElementById("principal").focus();
+  			} 
+		else {
+        		var Interest = (Principal * Years * Rate)/100;
 
-        document.getElementById('result').innerHTML = PayoutYear(); 
+        		document.getElementById('result').innerHTML = PayoutYear(); 
 
-        var Result = "<br />If you deposit <span>" + Principal + "</span>,<br />" 
+        		var Result = "<br />If you deposit <span>" + Principal + "</span>,<br />" 
 
-        Result = Result + " at an interest rate of <span>" + Rate + "% </span>. <br />" 
-        Result = Result + "You will receive an amount of <span>" + Interest + "</span>,<br />" 
-        Result = Result + "in the year <span>" + PayoutYear() + "</span>"; 
+			Result = Result + " at an interest rate of <strong>" + Rate + "% </strong>. <br />" 
+			Result = Result + "You will receive an amount of <span>" + Interest + "</span>,<br />" 
+			Result = Result + "in the year <span>" + PayoutYear() + "</span>"; 
 
-        
-
-        document.getElementById('result').innerHTML = Result
+        		document.getElementById('result').innerHTML = Result
 			
 		}
 		}
